06b237769504944b10b26d85c60b908f119ef9fa
scoliosis50 <148501880+scoliosis50@users.noreply.github.com>
2023-10-19 21:41:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-n
-n
+const coll = document.querySelectorAll(".collapsible")
+let i;
+
+for(i=0; I <coll.length; i++){
+  coll[I].addEventListener("click", function() {
+    this.classList.toggle("activate);
+    const content = this.nextElementSibling
+    if (content.style.display === "block) {
+        content.style.display = "none;
+  } else {
+    content.style.display = "block";
+  }
+});
+}
+  
+    
